36dd5a523e11bdbe347ade0b3f183baed04ae62a
LeoCobas <leo.cobas@gmail.com>
2026-04-15 00:16:23 -0300

    feat: remove tusFacturas API configuration from environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,5 @@ export const environment = {
   supabase: {
     url: 'https://ifkfofyylfkxwtxvyewi.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlma2ZvZnl5bGZreHd0eHZ5ZXdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0NDM0MTYsImV4cCI6MjA5MTAxOTQxNn0.Hjk3DasdvKTNgQqmu-RB1O-PIeFV-t7nHQcd9stPHUo'
-  },
-  tusFacturas: {
-    apiUrl: 'https://www.tusfacturas.com.ar/app/api',
   }
 };
